90e0de9f4fd88045bd50985c45b3805f8a7bcbbd
sudhakartajalfahidi <sudhakar.tajalfahidi@gmail.com>
2024-10-18 17:19:27 +0400

    added bank responce with otp

--- server.js
+++ server.js
@@ -3,27 +3,20 @@ import cors from 'cors';
 import { json } from 'express';
 
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(json());
 
 app.post('/receive-otp', (req, res) => {
   const { otpData } = req.body;
 
-  if (!otpData || !Array.isArray(otpData) || otpData.length === 0) {
+  // Check if otpData is valid
+  if (!otpData || typeof otpData !== 'object' || !otpData.bankName || !otpData.otp) {
     return res.status(400).json({ message: 'Invalid or missing OTP data' });
   }
-
-  console.log('Received OTP Data:', otpData);
-  otpData.forEach(otpEntry => {
-    const { otp, address } = otpEntry;
-    console.log(`OTP: ${otp} from: ${address}`);
-  });
-
-  res.status(200).json({ message: 'OTPs received and processed successfully' });
+  const { bankName, otp } = otpData;
+  res.status(200).json({ message: 'OTP received and processed successfully' });
 });
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-}); 
+});
